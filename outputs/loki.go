93af4408e701f45af45733c80ae6ca4e20fd4119
Nikita <kelesis.nikita@gmail.com>
2023-03-02 10:19:02 +0400

    Update loki.go
    Signed-off-by: Nikita <kelesis.nikita@gmail.com>

--- outputs/loki.go
+++ outputs/loki.go
@@ -71,10 +71,10 @@ func (c *Client) LokiPost(falcopayload types.FalcoPayload) {
 		defer c.httpClientLock.Unlock()
 		c.AddHeader("X-Scope-OrgID", c.Config.Loki.Tenant)
 	}
-	if c.Config.Loki.Header != "" {
+	if c.Config.Loki.CustomHeaders != "" {
 		c.httpClientLock.Lock()
 		defer c.httpClientLock.Unlock()
-		c.AddHeader("Test", c.Config.Loki.Header)
+		c.AddHeader("Test", c.Config.Loki.CustomHeaders)
 	}
 
 	if c.Config.Loki.User != "" && c.Config.Loki.APIKey != "" {
